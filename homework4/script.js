8003b132fa48758068a93d4693b42f98533de6a1
Alyona <60003653+alenushka0585@users.noreply.github.com>
2024-04-10 09:11:21 +0200

    second commit

--- homework4/script.js
+++ homework4/script.js
@@ -56,3 +56,14 @@ for (let i = 0; i < array2.length; i++) {
     console.log(array2[i].title);
    }
 }
+
+// Task #3____________________________________________________
+
+const array3 = [];
+for (let i = 0; i < array2.length; i++) {
+    if (array2[i].price < 1000) {
+     array3.push(array2[i]);
+    }
+ }
+
+ console.log(array3);
